100c36c6aafb5958282cf65371811199d4ea7e7f
Venu1433697 <canikissu687@gmail.com>
2025-02-17 12:43:05 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -29,3 +29,21 @@ Sequelize.sync()
 app.listen(3000,(res,rej)=>{
 console.log('3000 port')
 })
+
+const { Client } = require('pg');
+
+const client = new Client({
+	  host: 'your_rds_endpoint',
+	  port: 5432,
+	  user: 'your_db_user',
+	  password: 'your_db_password',
+	  database: 'your_db_name',
+});
+
+client.connect();
+
+client.query('SELECT NOW()', (err, res) => {
+	  console.log(err, res);
+	  client.end();
+});
+
